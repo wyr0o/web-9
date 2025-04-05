9c52233a636d7f3bad62c4cb293dba4d5ba0d407
wyr0o <brykin-2006@mail.ru>
2025-04-05 12:29:14 +0300

    Add files via upload

---
+++ вебинар 9 задание 3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void print(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout<< std::endl;
+}
+
+void reverse(int* arr, int size)
+{
+		for (int i = 0; i < size / 2; ++i)
+		{
+			int temp = arr[i];
+			arr[i] = arr[size - 1 - i];
+			arr[size - 1 - i] = temp;
+		}
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int arr [] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	std::cout << "до функции reverse: ";
+	print(arr, size);
+	reverse(arr, size);
+	std::cout << "после функции reverse: ";
+	print(arr, size);
+
+}
