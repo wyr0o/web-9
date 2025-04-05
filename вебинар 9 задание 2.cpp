439db706d4fed2f68b3ae9d14f9e75ad52ecd4aa
wyr0o <brykin-2006@mail.ru>
2025-04-05 12:10:24 +0300

    Add files via upload

---
+++ вебинар 9 задание 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void swap(int* num1, int* num2)
+{
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
+
+int main()
+{
+	int a = 8;
+	int b = 5;
+
+	std::cout << "a = " << a << ", b = " << b<< std::endl;
+
+	swap(&a, &b);
+
+	std::cout << "a = " << a << ", b = " << b<< std::endl;
+
+}
